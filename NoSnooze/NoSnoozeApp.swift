9a187aee2c06cabd8a51070ab5fa2c7cfdfcaf7b
Miguel <miguel@miguelchavezweb.com>
2023-07-28 05:41:50 -0400

    Initial Commit

---
+++ NoSnooze/NoSnoozeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoSnoozeApp.swift
+//  NoSnooze
+//
+//  Created by Miguel Chavez on 7/28/23.
+//
+
+import SwiftUI
+
+@main
+struct NoSnoozeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
